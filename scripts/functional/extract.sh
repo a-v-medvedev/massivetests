36870ea2ba675f0e80cdb9a2990121945d5869a7
Alexey V. Medvedev <alexey.v.medvedev@gmail.com>
2021-05-17 14:22:31 +0300

    Draft for a fucntional script added.

---
+++ scripts/functional/extract.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+#
+#   This file is part of massivetest.
+#
+#   Massivetest is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Foobar is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+#
+#
+
+source ./massive_tests.inc
+source ./params.inc
+
+check_bash_func_declared get_value
+
+
+function getline() {
+    local file=$1
+    local keywords="$2"
+    local key_block="$3"
+	local sizekeyword="$4"
+    local n="$5"
+    local size="$6"
+    local search="/[ {]n: $n,/ && /[ {]${sizekeyword}: [\"]*${size}[\"]*,/"
+    for i in 1 2 3 4; do
+        keyword=$(elem "$keywords" "$i" :)
+        if [ "$keyword" != "X" ]; then
+            key=$(elem "$key_block" "$i" :)
+            x="/[ {]${keyword}: [\"]*${key}[\"]*,/"
+            search="${search} && ${x}"
+        fi
+    done
+	cat "$file" | awk "$search { print }"
+}
+
+DIR="$1"
+PARAMS="$2"
+KEYWORDS="$3"
+SIZEKEYWORD="$4"
+
+[ ! -d "$DIR" ] && echo "ERROR: No argument or directory for extraction does not exist (dir=$DIR)" && exit 1
+[ ! -f "$DIR/output.yaml" ] && echo "ERROR: No output.yaml file in the directory for extraction (dir=$DIR))" && exit 1
+
+for p in $PARAMS; do
+    echo $p ":"
+    echo "---"
+    for n in $MASSIVE_TESTS_NODES; do
+        # FIXME add ppn handling
+        for size in $MASSIVE_TESTS_SIZES; do
+            L=$(getline $FIRST/output.yaml "$KEYWORDS" "$p" "$SIZEKEYWORD" "$n" "$size")
+            V=$(get_value "$L") 
+            [ -z "$V" ] && echo "ERROR: No data for {n=${n},size=${size}} in output.yaml file in the directory for extraction ($DIR)" && exit 1
+            echo $n $size $V
+        done
+    done
+    echo "---"
+done
---
+++ scripts/functional/functional_massive_tests.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#
+#   This file is part of massivetest.
+#
+#   Massivetest is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Foobar is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+#
+#
+
+source ./massive_tests.inc
+
+init_env
+
+source ./params.inc
+source ./modeset.inc
+
+check_bash_func_declared set_specific_params
+
+MASSIVE_TESTS_WORKLOADS="$WORKLOADS"
+MASSIVE_TESTS_CONFS="$CONFS"
+MASSIVE_TESTS_SECTIONS="$SECTIONS"
+MASSIVE_TESTS_PARAMETERS="$PARAMETERS"
+MASSIVE_TESTS_EXEC_REPEATS=$MASSIVE_TESTS_EXEC_REPEATS
+TUPLE=$(comb4 "$WORKLOADS" "$CONFS" "$SECTIONS" "$PARAMETERS")
+for mode in $MODES; do
+    for submode in $SUBMODES; do
+        CONF=conf.${mode}_${submode}
+        set_specific_params "$mode" "$submode"
+        massivetest-run
+        move_results "$CONF"
+        ./extract.sh "$CONF" "XXX" "$MASSIVE_TESTS_KEYWORDS" "$MASSIVE_TESTS_SIZEKEYWORD" > "out.$CONF" && true
+        if [ "$?" != "0" ]; then
+            tail -n1 "out.$CONF"
+            fatal "./extract.sh failed."
+        fi
+    done
+    NN=$(nelems "$MASSIVE_TESTS_NODES")
+    NS=$(nelems "$MASSIVE_TESTS_SIZES")
+    STEP=$(expr "$NN" \* "$NS" \+ 3)
+    ./script-postproc.sh conf.$mode "$STEP" "$TUPLE"
+done
+
+./make_table.sh "$TUPLE"
+
