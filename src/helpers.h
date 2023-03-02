9b989e0566f895134824f8698e7e2ee05f513040
Alexey V. Medvedev <alexey.v.medvedev@gmail.com>
2023-03-02 15:46:34 +0300

    Added temporary debug out.

--- src/helpers.h
+++ src/helpers.h
@@ -29,6 +29,7 @@
 #include <regex>
 #include <unistd.h>
 #include <assert.h>
+#include <sys/stat.h>
 #include "argsparser.h"
 
 namespace helpers {
@@ -52,6 +53,39 @@ static inline void subst(std::string &str, char pattern, char substitute) {
         str.replace(pos, 1, str_substitute);
         pos += 1; // skip the replaced character
     }
+}
+
+static inline std::string which(const std::string& command) {
+    static std::vector<std::string> dirs;
+    if (dirs.empty()) {
+        const char *path = std::getenv("PATH");
+        if (path == nullptr) {
+            dirs.push_back(".");
+        } else {
+            const char *start = path;
+            while (*start != '\0') {
+                const char *end = start;
+                while (*end != '\0' && *end != ':') {
+                    end++;
+                }
+                if (end > start) {
+                    dirs.emplace_back(start, end - start);
+                }
+                if (*end == ':') {
+                    end++;
+                }
+                start = end;
+            }
+        }
+    }
+    for (const auto& dir : dirs) {
+        std::string fullpath = dir + "/" + command;
+        struct stat st;
+        if (stat(fullpath.c_str(), &st) == 0 && (st.st_mode & S_IXUSR)) {
+            return fullpath;
+        }
+    }
+    return "";
 }
 
 static inline std::vector<std::string> str_split(const std::string &s, char delimiter) {
